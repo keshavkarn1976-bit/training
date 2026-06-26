a3cbcdb5d52811358a9e081d88d36f8c4c654669
keshavkarn1976-bit <keshavkarn1976@gmail.com>
2026-06-26 13:44:08 +0530

    Add async loadComponent function to load HTML

---
+++ js/common.js
@@ -0,0 +1,11 @@
+async function loadComponent(id, file) {
+
+    const response = await fetch(file);
+
+    const html = await response.text();
+
+    document.getElementById(id).innerHTML = html;
+
+}
+
+window.loadComponent = loadComponent;
